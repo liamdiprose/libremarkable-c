c73d19313fa87f44b3602330437013a8dacc379f
Liam Diprose <liam@liamdiprose.com>
2023-05-17 11:46:12 +1200

    Clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,15 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/release-22.11;
     flake-utils.url = github:numtide/flake-utils;
-
-
   };
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages."${system}";
     in {
-
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           cargo
-          rust-cbindgen
         ];
       };
     });
