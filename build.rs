c148ccea95c914838e4687c1884570f7ed7a0990
Liam Diprose <liam@liamdiprose.com>
2023-05-18 14:19:27 +1200

    Evaluate safer_ffi
    Unfortunately it can't derive_ReprC for re-exported structs (from what I
    can tell anyway)

--- build.rs
+++ build.rs
@@ -1,17 +1,17 @@
-extern crate cbindgen;
+// extern crate cbindgen;
 
 use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=cbindgen.toml");
+    // println!("cargo:rerun-if-changed=src/lib.rs");
+    // println!("cargo:rerun-if-changed=cbindgen.toml");
 
-    cbindgen::Builder::new()
-      .with_config(cbindgen::Config::from_file("cbindgen.toml").expect("Unable to open cbindgen config"))
-      .with_crate(crate_dir)
-      .generate()
-      .expect("Unable to generate bindings")
-      .write_to_file("bindings.h");
+    // cbindgen::Builder::new()
+    //   .with_config(cbindgen::Config::from_file("cbindgen.toml").expect("Unable to open cbindgen config"))
+    //   .with_crate(crate_dir)
+    //   .generate()
+    //   .expect("Unable to generate bindings")
+    //   .write_to_file("bindings.h");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,49 @@
-#[no_mangle]
-pub extern "C" fn hello_world() {
+use ::safer_ffi::prelude::*;
+
+
+#[ffi_export]
+pub fn hello_world() {
     println!("Hello, world!");
+}
+
+#[derive_ReprC]
+pub use libremarkable::cgmath::Point2;
+
+use libremarkable::framebuffer::common;
+use libremarkable::framebuffer::FramebufferDraw;
+
+#[derive_ReprC]
+pub use libremarkable::framebuffer::core::Framebuffer;
+
+#[derive_ReprC]
+pub use libremarkable::framebuffer::common::mxcfb_rect;
+
+#[ffi_export]
+pub fn framebuffer_new() -> &mut Framebuffer {
+    Framebuffer::new()
+}
+
+#[derive_ReprC]
+#[repr(C)]
+pub enum Color {
+    Black,
+    Gray(u8)
+}
+
+#[ffi_export]
+pub fn framebuffer_draw_line(framebuffer: &mut Framebuffer, start: Point2<i32>, end: Point2<i32>, width: u32, v: Color) -> mxcfb_rect {
+
+    let c: common::color = match v {
+        Color::Black => common::color::BLACK,
+        Color::Gray(v) => common::color::GRAY(v)
+    };
+
+    framebuffer.draw_line(start, end, width, c)
+}
+
+#[cfg(feature = "headers")]
+pub fn generate_headers() -> std::io::Result<()> {
+    ::safer_ffi::headers::builder()
+        .to_file("libremarkable_c.h")
+        .generate()
 }
