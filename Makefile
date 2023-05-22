037fc185b76304c10542b288264a50bb37b9b036
Liam Diprose <liam@liamdiprose.com>
2023-05-22 14:12:29 +1200

    Initial implementation of cbindgen

--- Makefile
+++ Makefile
@@ -1,2 +1,24 @@
 target/debug/liblibremarkable_c.so: src/lib.rs cbindgen.toml build.rs
 	cargo build
+
+target/debug/liblibremarkable_c.a: src/lib.rs cbindgen.toml build.rs
+	cargo build
+
+test/bin/hello_world: test/hello_world.c target/debug/liblibremarkable_c.so
+	mkdir -p test/bin
+	gcc -o $@ -Ltarget/debug -llibremarkable_c test/hello_world.c
+
+test/bin/hello_world_static: test/hello_world.c target/debug/liblibremarkable_c.a
+	gcc -c -Wall -fpic $< -o hello_world.o
+	gcc -o $@ -static hello_world.o -Ltarget/debug -llibremarkable_c
+
+
+test_shared: test/bin/hello_world
+	LD_LIBRARY_PATH=target/debug $<
+
+test: test/bin/hello_world_static
+	$<
+
+.PHONY = clean
+clean:
+	rm -rf test/bin
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,34 @@
 pub extern "C" fn hello_world() {
     println!("Hello, world!");
 }
+
+pub use libremarkable::cgmath::Point2;
+use libremarkable::framebuffer::common;
+use libremarkable::framebuffer::FramebufferDraw;
+
+pub use libremarkable::framebuffer::core::Framebuffer;
+pub use libremarkable::framebuffer::common::mxcfb_rect;
+
+#[no_mangle]
+pub extern "C" fn framebuffer_new() -> *mut Framebuffer {
+    &mut Framebuffer::new() as *mut Framebuffer
+}
+
+#[repr(C)]
+pub enum Color {
+    Black,
+    Gray(u8)
+}
+
+#[no_mangle]
+pub extern "C" fn framebuffer_draw_line(framebuffer: &mut Framebuffer, start: Point2<i32>, end: Point2<i32>, width: u32, v: Color) -> mxcfb_rect {
+    let c: common::color = match v {
+        Color::Black => common::color::BLACK,
+        Color::Gray(v) => common::color::GRAY(v)
+    };
+
+    framebuffer.draw_line(start, end, width, c)
+}
+
+#[no_mangle]
+pub extern "C" fn
